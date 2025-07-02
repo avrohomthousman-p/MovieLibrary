71991fccbf230abcafe7ae1ee6cabdfaa6ba8326
avrohomthousman-p <avrohomthousman@gmail.com>
2025-07-02 18:26:56 -0400

    feat: setup simple options for main activity

--- app/src/main/java/com/greatmachine/movielibrary/MainActivity.kt
+++ app/src/main/java/com/greatmachine/movielibrary/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -16,18 +23,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SampleData()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Spacer(modifier = Modifier.height(48.dp))
+                Text("What would you like to do today?", fontSize = 24.sp)
+                Spacer(modifier = Modifier.height(24.dp))
+                BrowseButton()
+                Spacer(modifier = Modifier.height(16.dp))
+                FavoritesButton()
+            }
         }
     }
 
 
     @Composable
-    fun SampleData(){
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ){
-            Text("testing to see if it works")
+    fun FavoritesButton() {
+        Button(onClick = {
+            // TODO: navigate to favorites activity
+        }) {
+            Text("Browse Favourites")
+        }
+    }
+
+
+    @Composable
+    fun BrowseButton() {
+        Button(onClick = {
+            // TODO: navigate to browse activity
+        }) {
+            Text("Browse All")
         }
     }
 }
