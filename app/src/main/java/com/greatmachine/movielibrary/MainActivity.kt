2b6fc79b16760d55e8e8eb4d46167e2f0321212f
avrohomthousman-p <avrohomthousman@gmail.com>
2025-07-02 18:13:19 -0400

    feat: getting started with jetpack

--- app/src/main/java/com/greatmachine/movielibrary/MainActivity.kt
+++ app/src/main/java/com/greatmachine/movielibrary/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.greatmachine.movielibrary
-
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            SampleData()
+        }
+    }
+
+
+    @Composable
+    fun SampleData(){
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Text("testing to see if it works")
         }
     }
 }
